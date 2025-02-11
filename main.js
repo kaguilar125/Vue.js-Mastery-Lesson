51e4c4f5030980e0da134ab56147e9f95aba564c
kaguilar125 <113131954+kaguilar125@users.noreply.github.com>
2025-02-10 23:23:21 -0500

    3

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const product = 'Socks'
 const app = Vue.createApp({
-  data() {
-    return {
-        product: "Socks",
-        description: "this is a description of the product"
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
+        }
     }
-  }
 })
